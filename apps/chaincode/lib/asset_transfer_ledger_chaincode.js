e002070a77316fcf4a05998c967eee03de1621f9
DarioHesami <97382667+DarioHesami@users.noreply.github.com>
2022-06-20 16:10:12 -0400

    Update asset_transfer_ledger_chaincode.js

--- apps/chaincode/lib/asset_transfer_ledger_chaincode.js
+++ apps/chaincode/lib/asset_transfer_ledger_chaincode.js
@@ -34,7 +34,58 @@ class Chaincode extends Contract {
 		//  Note - passing a 'nil' value will effectively delete the key from state, therefore we pass null character as value
 		await ctx.stub.putState(ownerIdIndexKey, Buffer.from('\u0000'));
 	}
- 
+
+	
+	// *** ReadAsset returns the asset stored in the world state with given id ***
+	async ReadAsset(ctx, id) {
+		const assetJSON = await ctx.stub.getState(id); // get the asset from chaincode state
+		if (!assetJSON || assetJSON.length === 0) {
+			throw new Error(`Asset ${id} does not exist`);
+		}
+
+		return assetJSON.toString();
+	}
+	
+	
+	// *** delete - remove a asset key/value pair from state ***
+	async DeleteAsset(ctx, id) {
+		if (!id) {
+			throw new Error('Asset id must not be empty');
+		}
+
+		let exists = await this.AssetExists(ctx, id);
+		if (!exists) {
+			throw new Error(`Asset ${id} does not exist`);
+		}
+
+		// to maintain the owner~id index, we need to read the asset first and get its owner
+		let valAsbytes = await ctx.stub.getState(id); // get the asset from chaincode state
+		let jsonResp = {};
+		if (!valAsbytes) {
+			jsonResp.error = `Asset does not exist: ${id}`;
+			throw new Error(jsonResp);
+		}
+		let assetJSON;
+		try {
+			assetJSON = JSON.parse(valAsbytes.toString());
+		} catch (err) {
+			jsonResp = {};
+			jsonResp.error = `Failed to decode JSON of: ${id}`;
+			throw new Error(jsonResp);
+		}
+		await ctx.stub.deleteState(id); //remove the asset from chaincode state
+
+		// delete the index
+		let indexName = 'owner~id';
+		let ownerIdIndexKey = ctx.stub.createCompositeKey(indexName, [assetJSON.owner, assetJSON.assetID]);
+		if (!ownerIdIndexKey) {
+			throw new Error(' Failed to create the createCompositeKey');
+		}
+		//  Delete index entry to state.
+		await ctx.stub.deleteState(ownerIdIndexKey);
+	}
+
+	
 }
 
 module.exports = Chaincode;
